7058aa79791d644480aa620f79447ef268633fa1
chou950 <chou950@gmail.com>
2018-04-18 15:29:43 +0800

    version1

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test{
+	
+	public static void main(String[] args){
+		
+		int a=1, b=1;
+		
+		System.out.println("這是修改前的版本~")
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("a+b="+(a+b));
+		
+	}
+	
+}
