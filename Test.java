cc6d6a907a03621746519471883f4c580e54fe45
chou950 <chou950@gmail.com>
2018-04-18 15:35:37 +0800

    version1

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ public class Test{
 		
 		int a=1, b=1;
 		
-		System.out.println("這是修改前的版本~")
+		System.out.println("這是修改前的版本~");
 		System.out.println("a="+a);
 		System.out.println("b="+b);
 		System.out.println("a+b="+(a+b));
--- test.bat
+++ test.bat
@@ -1,5 +1,3 @@
 set PATH="C:\Program Files\Java\jdk1.8.0_121\bin"
-javac -version
-java -version
 javac Test.java
 java Test
